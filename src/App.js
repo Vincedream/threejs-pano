2bc1e26e611fae1d3e169cea76b0872b04611112
jingchuan <3161141062@qq.com>
2019-09-01 15:34:24 +0800

    feat: add base threejs demo

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Pano from './components/pano';
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Base /> */}
+      <Pano />
+      {/* <Pano /> */}
+    </div>
+  );
+}
+
+export default App;
